bc71904ff9361103d775f1067bcf43fa770a427a
Shinji Kitagawa <shinji.kitagawa@mediphone.jp>
2024-08-14 22:40:30 +0900

    LLM関連（フロント）

--- src/App.js
+++ src/App.js
@@ -21,7 +21,52 @@ function App() {
 
     fetchCSV();
   }, []);
-  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    // currentIndexが変更されたときにimageDataを更新
+    const loadImageData = async () => {
+      const response = await fetch(
+        `saved_json/imageData_yuyu10_${currentIndex}.json`
+      );
+      const data = await response.json();
+      setImageData(data); // imageDataを更新
+    };
+    loadImageData();
+
+    const fetchSummary = async () => {
+      const response = await fetch(
+        `saved_json/feedback_yuyu10_${currentIndex}.json`
+      ); // JSONファイルのパス
+      const data = await response.json();
+      console.log(data);
+      setSummary(data); // summaryにデータを設定
+    };
+    fetchSummary();
+
+    const loadChatHistory = async () => {
+      const response = await fetch(
+        `saved_json/discussion_yuyu10_${currentIndex}.json`
+      );
+      const jsonData = await response.json();
+      console.log(jsonData);
+      const messages = await Promise.all(
+        jsonData.map(async (item) => {
+          return typeof item === "string" ? JSON.parse(item) : item; // 文字列の場合のみJSON.parse
+        })
+      );
+      console.log(messages);
+      const answers = await messages.map((data) => ({
+        question: data.question,
+        answer: data.content[0].text,
+      }));
+      setChatHistory(answers);
+      // // setChatHistory((prev) => [
+      // //   ...prev,
+      // //   {question:, answer: response.data.content[0].text},
+      // ]); // チャット履歴を更新
+    };
+    loadChatHistory();
+  }, [currentIndex]);
 
   const handleNext = () => {
     setCurrentIndex((prevIndex) => (prevIndex + 1) % (rows.length / 4)); // 次のインデックスに切り替え
@@ -174,23 +219,6 @@ function App() {
   });
   const [summary, setSummary] = useState("");
 
-  const fetchData = async () => {
-    try {
-      const response = await axios.post(
-        "http://localhost:8000/api/fetch-data",
-        {imagePath: selectedImage}
-      );
-      setImageData((prevData) => ({
-        ...prevData,
-        [selectedImage]: {
-          characters: response.data.characters,
-          sceneData: response.data.sceneData,
-        },
-      }));
-    } catch (error) {
-      console.error("Error fetching data:", error);
-    }
-  };
   const updateKomaPathInImageData = () => {
     setImageData((prevData) => ({
       ...prevData, // prevDataを展開
@@ -216,14 +244,24 @@ function App() {
       },
     }));
   };
+
   const saveToCSV = async () => {
     try {
       updateKomaPathInImageData(); // ここを追加
-      await axios.post("http://localhost:8000/api/save-csv", {
+      const dataName = imagePathList.image1.split("/")[1];
+
+      await axios.post("http://localhost:8000/api/save-json", {
+        dataName: dataName,
+        currentIndex: currentIndex,
         komaPath: imagePathList.image1,
         imageData,
         summary,
       });
+      // await axios.post("http://localhost:8000/api/save-csv", {
+      //   komaPath: imagePathList.image1,
+      //   imageData,
+      //   summary,
+      // });
     } catch (error) {
       console.error("Error saving CSV:", error);
     }
@@ -281,6 +319,86 @@ function App() {
   const toggleBlur = () => {
     setIsBlurred(!isBlurred); // ブラー状態を切り替え
   };
+
+  const [results, setResults] = useState(null);
+
+  const handleSubmit = async () => {
+    const formData = new FormData();
+    updateKomaPathInImageData();
+    formData.append("komaPath", {komaPath: imagePathList[selectedImage]});
+
+    try {
+      const response = await axios.post(
+        "http://localhost:8000/api/analyze-image/",
+        {komaPath: imagePathList[selectedImage]}
+      );
+      setResults(response);
+      const contentData = response.data.content_data;
+      setImageData((prevData) => ({
+        ...prevData,
+        [selectedImage]: {
+          ...prevData[selectedImage],
+          characters: contentData.characters.map((char) => ({
+            character: char.character,
+            faceDirection: char.faceDirection,
+            position: char.position,
+            expression: char.expression,
+            serif: char.serif,
+            clothing: char.clothing,
+            isVisible: char.isVisible ? 1 : 0,
+          })), // response.dataをimageDataに追加
+          sceneData: contentData.sceneData,
+        },
+      }));
+    } catch (error) {
+      console.error("Error uploading file:", error);
+    }
+  };
+  const fetchOverallFeedback = async () => {
+    try {
+      const dataName = imagePathList.image1.split("/")[1];
+      const response = await axios.post("http://localhost:8000/api/feedback", {
+        dataName: dataName,
+        currentIndex: currentIndex,
+        imageData,
+        imagePathList,
+      });
+      // console.log("Feedback:", response.data.feedback); // フィードバックをコンソールに表示
+      setSummary(response.data);
+    } catch (error) {
+      console.error("Error fetching feedback:", error);
+    }
+  };
+  const [chatHistory, setChatHistory] = useState([]); // チャット履歴を保持する状態を追加
+  const [discussionResult, setDiscussionResult] = useState([]);
+
+  const handleDiscussionButtonClick = async (question) => {
+    const dataName = imagePathList.image1.split("/")[1];
+    const requestData = {
+      dataName: dataName,
+      currentIndex: currentIndex,
+      imagePathList,
+      imageData,
+      summary,
+      question,
+    };
+
+    try {
+      const response = await axios.post(
+        "http://localhost:8000/api/discussion",
+        requestData
+      ); // LLMにリクエストを送信
+      console.log(response);
+      setDiscussionResult(response.data);
+      // setChatHistory((prev) => [
+      //   ...prev,
+      //   {question, answer: response.data.content[0].text},
+      // ]); // チャット履歴を更新
+    } catch (error) {
+      console.error("Error fetching chat response:", error);
+    }
+  };
+
   return (
     <div className="container">
       <header className="header">
@@ -453,18 +571,66 @@ function App() {
       </div>
       <div className="summary-section">
         <h3>4コマまとめ</h3>
+        <button class="btn btn-outline" onClick={fetchOverallFeedback}>
+          4コマ全体の感想を取得
+        </button>
+        <br></br>
         <textarea
           value={summary}
           onChange={(e) => setSummary(e.target.value)}
         />
       </div>
+
       <div className="discussion-section">
         <h3>ディスカッション</h3>
-        <button>どう感じた？</button>
-        <button>かわいいのはだれ？</button>
-        <button>次はどうなると思う？</button>
-        <button>特に気になる表現は？</button>
-        <button>今までになかったような表現や新しい情報はありますか？</button>
+        <button
+          class="btn btn-outline"
+          onClick={() => handleDiscussionButtonClick("どう感じた？")}
+        >
+          どう感じた？
+        </button>
+        <button
+          class="btn btn-outline"
+          onClick={() => handleDiscussionButtonClick("かわいいのはだれ？")}
+        >
+          かわいいのはだれ？
+        </button>
+        <button
+          class="btn btn-outline"
+          onClick={() => handleDiscussionButtonClick("次はどうなると思う？")}
+        >
+          次はどうなると思う？
+        </button>
+        <button
+          class="btn btn-outline"
+          onClick={() => handleDiscussionButtonClick("特に気になる表現は？")}
+        >
+          特に気になる表現は？
+        </button>
+        <button
+          class="btn btn-outline"
+          onClick={() =>
+            handleDiscussionButtonClick(
+              "今までになかったような表現や新しい情報はありますか？"
+            )
+          }
+        >
+          今までになかったような表現や新しい情報はありますか？
+        </button>
+      </div>
+      {/* <textarea
+        value={discussionResult}
+        onChange={(e) => setDiscussionResult(e.target.value)}
+      /> */}
+      <br></br>
+      <div className="chat-section">
+        {chatHistory.map((chat, index) => (
+          <div key={index}>
+            <strong>質問:</strong> {chat.question}
+            <br />
+            <strong>回答:</strong> {chat.answer}
+          </div>
+        ))}
       </div>
     </div>
   );
